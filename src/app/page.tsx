ec5cd4e7a5574e157b8be66f654a8ed8467f684c
Kevin Weaver <kevinweaver2@gmail.com>
2023-09-06 18:33:37 -0700

    Update title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,26 @@
-import { Account } from '../components/Account'
-import { Balance } from '../components/Balance'
-import { BlockNumber } from '../components/BlockNumber'
-import { ConnectButton } from '../components/ConnectButton'
-import { Connected } from '../components/Connected'
-import { NetworkSwitcher } from '../components/NetworkSwitcher'
-import { ReadContract } from '../components/ReadContract'
-import { ReadContracts } from '../components/ReadContracts'
-import { ReadContractsInfinite } from '../components/ReadContractsInfinite'
-import { SendTransaction } from '../components/SendTransaction'
-import { SendTransactionPrepared } from '../components/SendTransactionPrepared'
-import { SignMessage } from '../components/SignMessage'
-import { SignTypedData } from '../components/SignTypedData'
-import { Token } from '../components/Token'
-import { WatchContractEvents } from '../components/WatchContractEvents'
-import { WatchPendingTransactions } from '../components/WatchPendingTransactions'
-import { WriteContract } from '../components/WriteContract'
-import { WriteContractPrepared } from '../components/WriteContractPrepared'
+import { Account } from "../components/Account";
+import { Balance } from "../components/Balance";
+import { BlockNumber } from "../components/BlockNumber";
+import { ConnectButton } from "../components/ConnectButton";
+import { Connected } from "../components/Connected";
+import { NetworkSwitcher } from "../components/NetworkSwitcher";
+import { ReadContract } from "../components/ReadContract";
+import { ReadContracts } from "../components/ReadContracts";
+import { ReadContractsInfinite } from "../components/ReadContractsInfinite";
+import { SendTransaction } from "../components/SendTransaction";
+import { SendTransactionPrepared } from "../components/SendTransactionPrepared";
+import { SignMessage } from "../components/SignMessage";
+import { SignTypedData } from "../components/SignTypedData";
+import { Token } from "../components/Token";
+import { WatchContractEvents } from "../components/WatchContractEvents";
+import { WatchPendingTransactions } from "../components/WatchPendingTransactions";
+import { WriteContract } from "../components/WriteContract";
+import { WriteContractPrepared } from "../components/WriteContractPrepared";
 
 export default function Page() {
   return (
     <>
-      <h1>wagmi + RainbowKit + Next.js</h1>
+      <h1>Hackathon Template</h1>
 
       <ConnectButton />
 
@@ -90,5 +90,5 @@ export default function Page() {
         <WriteContractPrepared />
       </Connected>
     </>
-  )
+  );
 }
